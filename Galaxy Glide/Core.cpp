b45c500cbf80bf768c32f69c7856e5377cb06bee
6ra1ju <22028307@vnu.edu.vn>
2025-07-24 04:14:50 +0700

    nothing

--- Galaxy Glide/Core.cpp
+++ Galaxy Glide/Core.cpp
@@ -5,7 +5,6 @@
 #include <cmath>
 #include "SpaceShip.hpp"
 #include "Fire.hpp"
-//new_comment
 SDL_Renderer* Core::gRenderer = nullptr;
 SDL_Window* Core::gWindow = nullptr;
 TTF_Font* Core::gFont = nullptr;
