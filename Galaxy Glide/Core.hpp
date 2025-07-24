b8cc5eba91bccc1ad4b57a12b034fc1852504e2d
6ra1ju <22028307@vnu.edu.vn>
2025-07-24 18:24:01 +0700

    test3

--- Galaxy Glide/Core.hpp
+++ Galaxy Glide/Core.hpp
@@ -1,12 +1,10 @@
 #ifndef CORE_H
 #define CORE_H
-// add comment again
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
 #include <vector>
-
 using namespace std;
 
 class SpaceShip;
